cad13df532e8b2a1da75a273bc98eb83409ee04c
Luka Alimbarashvili <lucas@Lucas-Macbook-Pro.local>
2020-04-28 12:13:09 +0400

    Registration

--- Homework9/Homework9.1/ViewController.swift
+++ Homework9/Homework9.1/ViewController.swift
@@ -27,32 +27,24 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-  
+    
+    func alert(title:String, message: String)  {
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK!", style: .cancel, handler: nil))
+        self.present(alert, animated: true)
+        
+    }
 
     @IBAction func registrationDone(_ sender: Any) {
         if name.text == "" || surname.text == "" || email.text == "" || phoneNumber.text == "" {
-            
-            let alert = UIAlertController(title: "Failed", message: "Please fill all the component", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK!", style: .cancel, handler: nil))
-            self.present(alert, animated: true)
-            
+            alert(title: "Failed", message: "Please fill all the component")
         }  else if !((email.text?.contains("@"))!)  {
-            
-            let alert = UIAlertController(title: "Failed", message: "use @ in email", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK!", style: .cancel, handler: nil))
-            self.present(alert, animated: true)
-            
+            alert(title: "Failed", message: "Use @ in email")
         } else if name.text!.count < 4 || surname.text!.count < 4 {
-            
-            let alert = UIAlertController(title: "Failed", message: "name and surname must be more than 3 character", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK!", style: .cancel, handler: nil))
-            self.present(alert, animated: true)
-            
+            alert(title: "Failed", message: "Name and Surname must be more than 3 character")
         } else if phoneNumber.text!.count < 9 {
-            let alert = UIAlertController(title: "Failed", message: "Phone number is incorrect", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK!", style: .cancel, handler: nil))
-            self.present(alert, animated: true)
-            
+            alert(title: "Failed", message: "Phone number is incorrect")
         } else {
             self.nameText = name.text!
             self.surnameText = surname.text!
