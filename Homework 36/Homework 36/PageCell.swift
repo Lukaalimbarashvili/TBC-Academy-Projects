b4df926ce3a4aa49774b0b7b5013151cdda54525
Luka Alimbarashvili <lucas@Lucas-Macbook-Pro.local>
2020-06-09 00:31:04 +0400

    Homework

---
+++ Homework 36/Homework 36/PageCell.swift
@@ -0,0 +1,18 @@
+//
+//  PageCell.swift
+//  Homework 36
+//
+//  Created by Luka Alimbarashvili on 6/8/20.
+//  Copyright © 2020 lucas. All rights reserved.
+//
+
+import UIKit
+
+class PageCell: UICollectionViewCell {
+    
+    @IBOutlet weak var mainImageView: UIImageView!
+    @IBOutlet weak var topic: UILabel!
+    @IBOutlet weak var detailInfo: UILabel!
+    @IBOutlet weak var greenBackground: UIImageView!
+    
+}
